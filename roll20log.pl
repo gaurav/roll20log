58f5083705abe8cc1632c8dd0cf2c4615f67ffe2
gaurav <gaurav@ggvaidya.com>
2014-09-28 09:40:35 -0600

    Really basic script to read roll20 logs.

---
+++ roll20log.pl
@@ -0,0 +1,167 @@
+#!env perl
+
+use v5.010;
+
+use strict;
+use warnings;
+
+use Try::Tiny;
+use Getopt::Long;
+use Pod::Usage;
+use Data::Dumper;
+
+use MIME::Base64;
+use JSON;
+
+# Process command-line arguments so we know what we're doing.
+my $help;
+my $man;
+
+my $flag_hide_metadata = 0;
+
+GetOptions(
+    'help|?' => \$help, 
+    man => \$man,
+
+    'hide-metadata' => \$flag_hide_metadata
+) or pod2usage(2);
+pod2usage(1) if $help;
+pod2usage(-exitval => 0, -verbose => 2) if $man;
+
+# Assume any files still on the path are .html files that need parsing.
+my @all_messages;
+
+foreach my $filename (@ARGV) {
+    try {
+        say STDERR "Reading HTML file '$filename'.";
+
+        open(my $fh, "<:encoding(utf-8)", $filename) 
+            or die "  Could not open '$filename': $!";
+
+        my $msgdata;
+        while(<$fh>) {
+            chomp;
+
+            if(/^\s*var msgdata = "([\w\+\/]+)";\s*$/) {
+                die "  Two msgdata lines in one file, what?" if defined $msgdata;
+                $msgdata = $1;
+            }
+        }
+
+        close($fh);
+
+        die "  No msgdata line found in file!" unless defined $msgdata; 
+
+        say STDERR "  msgdata of " . length($msgdata) . " characters found.";
+
+        my $json_source = MIME::Base64::decode_base64($msgdata);
+        my $json = JSON::decode_json($json_source);
+
+        # This is broken up into chunks for some reason. Hmm.
+        my $chunk_id = 0;
+        foreach my $chunk (@$json) {
+            $chunk_id++;
+
+            foreach my $message_key (sort keys %$chunk) {
+                my $message = $chunk->{$message_key};
+
+                $message->{'_id'} = $message_key;
+                $message->{'_chunk_id'} = $chunk_id;
+
+                push @all_messages, $message;
+            }
+        }
+
+    } catch {
+        warn $_;
+    }
+}
+
+say STDERR scalar(@all_messages) . " messages to process.";
+
+say STDERR "Writing all messages to STDOUT.";
+
+foreach my $message (@all_messages) {
+    my $type = $message->{'type'};
+    my $metadata = "";
+
+    $metadata = " [" . $message->{'_id'} . " #" . 
+        $message->{'_chunk_id'} . "]"
+        unless $flag_hide_metadata;
+
+    if($type eq 'general') {
+        say sprintf("%s%s: %s",
+            $message->{'who'},
+            $metadata,
+            $message->{'content'}
+        );
+    } elsif($type eq 'emote') {
+        say sprintf("%s%s %s",
+            $metadata,
+            $message->{'who'},
+            $message->{'content'}
+        );
+    } elsif($type eq 'rollresult' || $type eq 'gmrollresult') {
+        my @roll_details;
+        my $roll_content = JSON::decode_json($message->{'content'});
+
+        my $result_type;
+        given($roll_content->{'resultType'}) {
+            when('sum') { $result_type = " + "; }
+            default { die "Unknown result type: $_"; }
+        };
+
+        foreach my $roll (@{$roll_content->{'rolls'}}) {
+            given($roll->{'type'}) {
+                when('C') {
+                    # Count rolls greater than.
+                    push @roll_details, $roll->{'text'};
+                }
+                when('R') {
+                    push @roll_details, sprintf("%dd%d [%d]",
+                        $roll->{'dice'},
+                        $roll->{'sides'},
+                        join(', ', map { $_->{'v'} } @{$roll->{'results'}})
+                    );
+                }
+                when('M') {
+                    push @roll_details, $roll->{'expr'};
+                }
+                default {
+                    die "Unknown roll type: '$_': " . Dumper($roll_content);
+                }
+            }
+        }
+        
+        my $roll_summary = join(" ", @roll_details);
+        $roll_summary .= " = ";
+        $roll_summary .= $roll_content->{'total'};
+
+        say sprintf("%s%s rolls %s",
+            $message->{'who'},
+            $metadata,
+            $roll_summary
+        );
+    
+    } elsif($type eq 'desc') {
+        say sprintf("%s%s %s",
+            $message->{'who'},
+            $metadata,
+            $message->{'content'}
+        );
+
+    } elsif($type eq 'whisper') {
+        say sprintf("%s whispers to %s%s: %s",
+            $message->{'who'},
+            $message->{'target_name'},
+            $metadata,
+            $message->{'content'}
+        );
+
+    } elsif($type eq 'api') {
+        # These don't seem to appear? Ignore.
+
+    } else {
+        die "Unknown type '$type': " . Dumper($message);
+    }
+}
